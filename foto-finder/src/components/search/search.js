cdf464d252b8c456a36fc28338ee866834bae948
ausi1997.com <ausafen1018@gmail.com>
2021-03-13 17:59:45 +0530

    calling the external api

--- foto-finder/src/components/search/search.js
+++ foto-finder/src/components/search/search.js
@@ -1,13 +1,28 @@
 import React , {Component} from "react";
-
+import axios from "axios";
 class Search extends Component{
     state = {
         searchText : '',
         apiUrl : "https://pixabay.com/api/",
         apiKey : "19039698-70d84fc349952a4a57cf2d93d",
         images :[]
+    };
+
+    onTextChange = (e)=>{
+        this.setState({[e.target.name]:e.target.value},()=>{
+            axios.get(
+                `${this.state.apiUrl}/key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&safesearch=true`
+            ).then((res)=>{
+                this.setState({images:res.data.hits})
+            }).catch(err=>{
+                console.log(err);
+            })
+        }
+        )
     }
+
     render(){
+        console.log(this.state.images);
         return(
             <div>
             <input type="text" style = {{backgroundColor:"black",
@@ -21,7 +36,9 @@ class Search extends Component{
                 borderRightStyle:"hidden",
                 borderLeftStyle:"hidden",
                 outline:"none",
-                borderBottomStyle:"groove"}} placeholder= "Search for images..." name = "searchText" value={this.state.searchText} ></input>
+                borderBottomStyle:"groove"}} placeholder= "Search for images..." name = "searchText" value={this.state.searchText}
+                onChange = {this.onTextChange} ></input>
+                {this.state.images.length>0?(<Image images={this.state.images}></Image>):null}
             </div>
           
         )
