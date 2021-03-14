22bb06cee3ea7554aa333cc2e33282b5d8fddaab
ausi1997.com <ausafen1018@gmail.com>
2021-03-14 13:59:45 +0530

    zoom-in image

--- foto-finder/src/components/image/image.js
+++ foto-finder/src/components/image/image.js
@@ -8,6 +8,16 @@ import FlatButton from "material-ui/FlatButton";
 
 
 class Image extends Component{
+    state = {
+        open:false,
+        currentImg:""
+    }
+    handleOpen = (img)=>{
+        this.setState({open:true, currentImg:img})
+    }
+    handleClose = ()=>{
+        this.setState({open:false})
+    }
     render(){
         let imageList;
         const {images} = this.props;
@@ -18,7 +28,7 @@ class Image extends Component{
            {
                images.map(img=>{
                    return<GridTile title={img.tags} key={img.id} actionIcon={
-                       <IconButton>
+                       <IconButton onClick={()=>this.handleOpen(img.largeImageURL)}>
                        <ZoomIn color="white"></ZoomIn>
                        </IconButton>
                     }>
@@ -31,9 +41,14 @@ class Image extends Component{
         else{
             imageList=null;
         }
+        const action = [<FlatButton label="Close" primary={true} onClick={this.handleClose}>
+            </FlatButton>]
+            
         return(
-            <div>
+            <div style = {{marginLeft:50,marginRight:50,marginTop:20}}>
             {imageList}
+            <Dialog actions={action} modal={false} open={this.state.open} onRequestClose={this.handleClose}></Dialog>
+            <img src={this.state.currentImg} alt="..." style = {{width:"100&"}}></img>
             </div>
         )
     }
